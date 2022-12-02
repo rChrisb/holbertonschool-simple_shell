22a29a4fb2bc5700a02b3c037b991d9ca23c0b68
rChrisb <5767@holbertonstudents.com>
2022-12-02 15:59:38 +0100

    update

--- main.c
+++ main.c
@@ -8,15 +8,20 @@
 void prompt(char p[10]);
 char * _getline(void);
 void exec(char *buf, char *av[]);
+char **split_line(char *buff);
+
+
 int main(__attribute__((unused)) int ac, char *av[])
 {
 	while (1)
 	{
 	prompt("$£$ ");
 	char *buffer = _getline();
+	char **tokens = split_line(buffer);
 	//strtok
 	//stat
-	exec(buffer, av);
+
+	exec(buffer, tokens);
 	}
 	return (0);
 }
