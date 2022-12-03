a39f8db0c0011d17c4bb35ded31a033269a28627
rChrisb <5767@holbertonstudents.com>
2022-12-03 14:16:48 +0100

    function pointer to get the right function to exec

---
+++ builtingetter.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * getfunc - gets the right function for the input command
+ * @s: input command
+ *
+ * Return: function to execute
+ */
+
+int (*getfunc(char *s))(void)
+{
+	builtin builts[] = {
+		{"exit", _exitshell},
+		{"env", _printenv},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while ((builts[i].function != NULL) && (strcmp(builts[i].command, s) != 0))
+	{
+		i++;
+	}
+	return (builts[i].function);
+}
