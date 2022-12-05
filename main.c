0ee7d841a91203c08fb18124432c5a1d00ce570b
rChrisb <5767@holbertonstudents.com>
2022-12-05 17:12:53 +0100

    Main file

--- main.c
+++ main.c
@@ -4,13 +4,16 @@
  * main - Entry point
  * @ac: number of arguments in command line
  * @av: values of arguments
- *
+ * @env: global environment
  * Return: 0
  */
 
-int main(__attribute__((unused)) int ac, __attribute__((unused)) char *av[], char **env)
+int main(int ac, char *av[], char **env)
 {
-	char *buffer = NULL/*, **tokens*/; 
+	char *buffer = NULL/*, **tokens*/;
+
+	(void)ac;
+	(void)av;
 
 	while (1)
 	{
