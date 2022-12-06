8af643e3472e9e078ee514be50b235a5bc58e6e4
rChrisb <5767@holbertonstudents.com>
2022-12-06 15:47:21 +0100

    Header file

--- main.h
+++ main.h
@@ -34,6 +34,7 @@ void exec(char *buf, char *av[], char **env);
 char **split_line(char *buff);
 char *searchpath(char *buffer, char *path);
 void freetoken(char **tokens);
+int ignorespace(char *buffer);
 void execute(char *buff, char **env);
 
 #endif
